54e55c534fd06faf710bef0a15e0a61ee17d179d
salad74 <salad74@ville-noumea.nc>
2012-04-19 23:30:16 +0000

    Première version stable, imoportée des sources originales, passées en UTF-8, converties en projet maven, avec quelques corrections sur les imports

---
+++ framework-1.0/src/main/java/nc/mairie/technique/MairiePDF.java
@@ -0,0 +1,39 @@
+package nc.mairie.technique;
+
+import java.util.Hashtable;
+/**
+ * Insérez la description du type ici.
+ * Date de création : (27/02/2004 14:45:11)
+ * @author: Administrator
+ */
+public class MairiePDF {
+	private java.lang.String nomMasque;
+	private java.util.Hashtable hashChamps;
+/**
+ * Commentaire relatif au constructeur MairiePDF.
+ */
+public MairiePDF(String nomMasque, Hashtable hashChamps) {
+	super();
+	this.nomMasque = nomMasque;
+	this.hashChamps = hashChamps;
+}
+/**
+ * Insérez la description de la m�thode ici.
+ *  Date de création : (27/02/2004 14:48:20)
+ * @return java.util.Hashtable
+ */
+public java.util.Hashtable getHashChamps() {
+	if (hashChamps == null) {
+		hashChamps = new java.util.Hashtable();
+	}
+	return hashChamps;
+}
+/**
+ * Insérez la description de la m�thode ici.
+ *  Date de création : (27/02/2004 14:45:56)
+ * @return java.lang.String
+ */
+public java.lang.String getNomMasque() {
+	return nomMasque;
+}
+}
